672e5243f893b978aaca75e31827aea26a3a0b81
PitorParker <huberpetergyorgy@gmail.com>
2023-12-12 18:31:04 +0100

    header

--- components/header.php
+++ components/header.php
@@ -1,14 +1,11 @@
 <div class="nav-fixed">
   <nav class="custom-navbar">
-
-        <img src="imgs/logo.png" alt="" title="" class="logo">
-
-          <form action="#" class="custom-search">
-                      <input type="text" placeholder="Keresés..." name="search" />
-                      <button><i class="fa-solid fa-magnifying-glass"></i></button>
-          </form>
-
-          <button class="loginBtn btnLogin-popup">Belépés <i class="fa-solid fa-right-to-bracket"></i></button>
+  <a href="index.php"><img src="imgs/logo.png" alt="" title="" class="logo"></a>
+    <form action="#" class="custom-search">
+      <input type="text" placeholder="Keresés..." name="search" />
+      <button><i class="fa-solid fa-magnifying-glass"></i></button>
+    </form>
+    <button class="loginBtn btnLogin-popup">Belépés <i class="fa-solid fa-right-to-bracket"></i></button>
   </nav>
 
   <nav class="category">
@@ -20,5 +17,6 @@
       <li><a href="#">ASD4</a></li>
       <li><a href="#">ASD5</a></li>
     </ul>
+    <a class="kapcsolat" href="contact.php">Kapcsolat</a>
   </nav>
 </div>
