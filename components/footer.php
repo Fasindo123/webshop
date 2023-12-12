a5866b7fb978cbbcd6e6c6b000da5ef0eb7cec1f
Gerri001 <97045739+DrRyters@users.noreply.github.com>
2023-12-12 13:01:48 +0100

    webshop

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,46 @@
-<footer>
-    
+<link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/535ee732fe.js" crossorigin="anonymous"></script>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+        <div class="footer-col">
+            <h4><i class="fa-solid fa-folder-open"></i> Menü</h4>
+            <ul>
+            <li><a href="#">Szervezetünk</a></li>
+            <li><a href="#">Képzéseink</a></li>
+            <li><a href="#">Oktatóink</a></li>
+            <li><a href="#">Infrastruktúránk</a></li>
+            <li><a href="#">Kapcsolat</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4><i class="fa-solid fa-envelope"></i> Kapcsolatfelvétel</h4>
+            <ul>
+            <li><a href="contact.php">Kapcsolat <i class="fa-solid fa-share-from-square"></i></a></li>
+            <li><i class="fa-solid fa-location-dot"></i> Cím: 7400 Kaposvár, <br>
+                Damjanich utca 17.</li>
+            <li><i class="fa-solid fa-phone"></i> Telefon: (+36) 30 092 2911</li>
+            <li><i class="fa-solid fa-envelope"></i> E-mail: levike.pinter@gmail.com</li>
+            <li><i class="fa-solid fa-clock"></i> Nyitva: Hétfő - Péntek<br>8:00 - 16:00</li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4><i class="fa-solid fa-scroll"></i> Ügyfélszolgálat</h4>
+            <ul>
+            <li><a href="#">Gyik</a></li>
+            <li><a href="#">Adatvédelmi Nyilatkozat</a></li>
+            </ul> <br>
+            <h4><i class="fa-solid fa-credit-card"></i> Fizetési módok</h4>
+            <img class="ms-img ms-img--fluid" src="//assets.mmsrg.com/isr/166325/c1/-/ms-cms-mmhu-l16446034/feecms_x_x_x" srcset="//assets.mmsrg.com/isr/166325/c1/-/ms-cms-mmhu-l16446034/feecms_x_x_x 1x">
+        </div>
+        <div class="footer-col">
+            <h4><i class="fa-solid fa-share-nodes"></i> Közösségi Média</h4>
+            <div class="social-links">
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+            </div>
+        </div>
+        </div>
+    </div>
 </footer>
