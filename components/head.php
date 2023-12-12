6cde7763781c0ef4ea2af6812da1e7cde2af08c0
PitorParker <106076392+PitorParker@users.noreply.github.com>
2023-12-12 12:59:34 +0100

    navbar

--- components/head.php
+++ components/head.php
@@ -1,8 +1,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ASD</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="">
+    <title>TechTrendStore</title>
 
     <link rel="stylesheet" href="style.css">
-    
+    <script src="https://kit.fontawesome.com/535ee732fe.js" crossorigin="anonymous"></script>
 </head>
--- components/header.php
+++ components/header.php
@@ -1,27 +1,21 @@
-<link rel="stylesheet" href="style.css">
-<header>
-<div class="header-icons">
-<img src="logo.png" alt="Logo">
-    <input type="text" placeholder="Keresés">
-    <a href="#">Ügyfél fiók</a>
-    <a href="#">Kosár</a>
-  </div>
-</header>
-<header >
-    <nav>
-      <ul>
-          <li><a href="#">Összes</a></li>
-          <li><a href="#">Termék 1</a></li>
-          <li><a href="#">Termék 2</a></li>
-          <li><a href="#">Termék 3</a></li>
-          <li><a href="#">Termék 4</a></li>
-          <li><a href="#">Termék 5</a></li>
-          <li><a href="#">Termék 6</a></li>
-          <li><a href="#">Termék 7</a></li>
-          <li><a href="#">Termék 8</a></li>
+<nav class="custom-navbar">
 
+      <img src="imgs/logo.png" alt="" title="" class="logo">
 
-      <li><a href="#">Kapcsolat</a></li>
-      </ul>
-    </nav>
-</header>
+        <form action="#" class="custom-search">
+                    <input type="text" placeholder="Keresés..." name="search" />
+                    <button><i class="fa-solid fa-magnifying-glass"></i></button>
+        </form>
+
+        <button class="loginBtn">Belépés <i class="fa-solid fa-right-to-bracket"></i></button>
+</nav>
+
+<nav class="category">
+  <ul>
+    <li>asd</li>
+    <li>asd</li>
+    <li>asd</li>
+    <li>asd</li>
+    <li>asd</li>
+  </ul>
+</nav>
