15f90760d927019c5e082d9794acab3346739432
DrRyters <97045739+DrRyters@users.noreply.github.com>
2023-12-12 17:20:35 +0100

    contact kezdete

---
+++ contact.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="hu">
+    <title>Kapcsolat - TechTrendStore</title>
+<?php require_once('components/head.php'); ?>
+<body>
+      <?php require_once('components/header.php'); ?>
+      <?php require_once('components/login.php'); ?>
+      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+      <button id="backToTopBtn" onclick="scrollToTop()">
+        <i class="fa-solid fa-chevron-up"></i>
+      </button>
+
+    <script src="script.js"></script>
+    <?php require_once("components/footer.php"); ?>
+  </div>
+
+  </body>
+</html>
