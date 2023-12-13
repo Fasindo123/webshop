b79b6458ce13263585b317edbd9ff4aa7f0acb10
L3v3nt34627 <levike.pinter@gmail.com>
2023-12-13 12:49:57 +0100

    asd

--- checkout.php
+++ checkout.php
@@ -3,8 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="checkout.css">
+
+    <link rel="shortcut icon" type="image/x-icon" href="imgs/t_white.png">
+    <title>TechTrendStore</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script scr="script.js"></script>
+    <script src="https://kit.fontawesome.com/535ee732fe.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../bootstrap.min.css">
+    <script src="../bootstrap.min.css" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="iphone">
