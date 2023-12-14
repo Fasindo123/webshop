4baea80728c38f650fb05b1c0849c142156495bf
Gerri001 <97045739+DrRyters@users.noreply.github.com>
2023-12-14 13:10:56 +0100

    asdadsadd

--- components/header.php
+++ components/header.php
@@ -1,5 +1,5 @@
-<div class="nav-fixed">
-  <nav class="custom-navbar">
+<div class="fixed-top mb-5">
+  <!-- <nav class="custom-navbar">
       <div>
         <a href="index.php"><img src="imgs/tts.png" alt="logo" title="" class="logo"></a>
       </div>
@@ -15,15 +15,51 @@
         <a href="contact.php"><i class="fa-solid fa-envelope"></i></a>
         <a href=""><i class="fa-solid fa-user"></i></a>
       </div>
-  </nav>
+  </nav> -->
 
-  <nav class="category">
+  <!-- <nav class="category">
     <ul>
       <li><a href="#">ASD1</a></li>
       <li><a href="#">ASD2</a></li>
       <li><a href="#">ASD3</a></li>
       <li><a href="#">ASD4</a></li>
       <li><a href="#">ASD5</a></li>
+    </ul>
+  </nav> -->
+
+  <nav class="navbar navbar-expand-lg custom-navbar">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><img src="imgs/tts.png" alt="TechTrendStore Logó" title="TechTrendStore Logó" class="logo"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse custom-search" id="navbarSupportedContent">
+      <form class="d-flex justify-content-center" role="search" style="width: 60%; margin: 0 auto;">
+        <input class="form-control me-2 custom-search-input" type="search" placeholder="Keresés..." aria-label="Search" style="border-radius: 15px; font-weight: 600;">
+        <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+      </form>
+
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="favourite.php"><i class="fa-solid fa-heart"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php"><i class="fa-solid fa-envelope"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa-solid fa-user"></i></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<nav class="category" >
+    <ul>
+
     </ul>
   </nav>
 </div>
