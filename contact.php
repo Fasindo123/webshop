8265f9addcefa8bcd785226b770b5fa5b3706e41
Gerri001 <97045739+DrRyters@users.noreply.github.com>
2023-12-14 08:31:11 +0100

    contact fél működő

--- contact.php
+++ contact.php
@@ -1,47 +1,47 @@
 <!DOCTYPE html>
 <html lang="hu">
-    <title>Kapcsolat - TechTrendStore</title>
+    <title>TechTrendStore - Kapcsolat</title>
 <?php require_once('components/head.php'); ?>
 <body>
   <?php require_once('components/header.php'); ?>
-
+  <?php require_once('mail/zsotamail.php'); ?>
   <br><br><br>
-
-            <div class="screen-body container">
-              <div class="screen-body-item left">
-                <div class="app-title">
-                  <span><i class="fa-solid fa-envelopes-bulk"></i> Kapcsolatfelvétel</span>
-                </div>
-                <div class="app-contact"><i class="fa-solid fa-phone"></i> <a href="tel:06300922911"> 06 30 092 2911</a></div>
-                <div class="app-contact"><i class="fa-solid fa-envelope"></i> <a href="mailto:levike.pinter@gmail.com">levike.pinter@gmail.com</a></div>
-                <div class="app-contact"><i class="fa-solid fa-location-dot"></i> 7400 Kaposvár, Damijanich utca 17.</div>
-              </div>
-              
-              <div class="screen-body-item">
-                <div class="app-form">
-                  <div class="app-form-group">
-                    <input class="app-form-control" placeholder="Név" id="nev">
-                  </div>
-                  <div class="app-form-group">
-                    <input class="app-form-control" placeholder="E-mail" id="email">
-                  </div>
-                  <div class="app-form-group">
-                    <input class="app-form-control" placeholder="Telefonszám" id="phone-number" oninput="formatPhoneNumber(this)">
-                  </div>
-                  <div class="app-form-group message">
-                    <textarea class="app-form-control" placeholder="Üzenet"></textarea>
-                  </div>
-
-                  <div>
-                    <button class="kuldes" id="submit" onclick="sendForm()">Küldés <i class="fa-solid fa-paper-plane"></i></button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="block-map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2753.369275933826!2d17.7972176!3d46.362061000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47683e000240052b%3A0x3b1f6a2a20645b22!2zS2Fwb3N2w6FyaSBTWkMgRcO2dHbDtnMgTG9yw6FuZCBNxbFzemFraSBUZWNobmlrdW0gw6lzIEtvbGzDqWdpdW0!5e0!3m2!1shu!2shu!4v1698227964602!5m2!1shu!2shu" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <form action="contact.php" method="post">
+      <div class="screen-body container">
+        <div class="screen-body-item left">
+          <div class="app-title">
+            <span><i class="fa-solid fa-envelopes-bulk"></i> Kapcsolatfelvétel</span>
           </div>
+          <div class="app-contact"><i class="fa-solid fa-phone"></i> <a href="tel:06300922911"> 06 30 092 2911</a></div>
+          <div class="app-contact"><i class="fa-solid fa-envelope"></i> <a href="mailto:levike.pinter@gmail.com">levike.pinter@gmail.com</a></div>
+          <div class="app-contact"><i class="fa-solid fa-location-dot"></i> 7400 Kaposvár, Damijanich utca 17.</div>
+        </div>
+        
+        <div class="screen-body-item">
+          <div class="app-form">
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="Név" id="nev" name="nev" required>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="E-mail" id="email" name="email" required>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="Telefonszám" id="phone-number" name="phone-number" oninput="formatPhoneNumber(this)" required>
+            </div>
+            <div class="app-form-group message">
+              <textarea class="app-form-control" placeholder="Üzenet" name="uzenet" required></textarea>
+            </div>
 
+            <div>
+              <button type="button" class="kuldes" id="Submit" name="Submit" onclick="sendForm()">Küldés <i class="fa-solid fa-paper-plane"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="block-map">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2753.369275933826!2d17.7972176!3d46.362061000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47683e000240052b%3A0x3b1f6a2a20645b22!2zS2Fwb3N2w6FyaSBTWkMgRcO2dHbDtnMgTG9yw6FuZCBNxbFzemFraSBUZWNobmlrdW0gw6lzIEtvbGzDqWdpdW0!5e0!3m2!1shu!2shu!4v1698227964602!5m2!1shu!2shu" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </form>
   <?php require_once('components/footer.php'); ?>
 </body>
 </html>
@@ -86,32 +86,40 @@
   input.setSelectionRange(newCursorPosition, newCursorPosition);
 }
 
+let isFormSubmitted = false;
+
 function sendForm() {
-    const elements = document.querySelectorAll('.app-form-control');
-    let hasErrors = false; // Flag to check if there are errors
-    const errorMessages = [];
+  if (isFormSubmitted) {
+    alert('Az űrlapot már elküldték!');
+    return false;
+  }
 
-    // Remove red border from all input fields
-    elements.forEach(element => {
-        element.style.border = '';
-    });
+  const elements = document.querySelectorAll('.app-form-control');
+  let hasErrors = false; // Flag to check if there are errors
+  const errorMessages = [];
 
-    let isAnyFieldEmpty = false; // Flag to check if any field is empty
+  // Remove red border from all input fields
+  elements.forEach(element => {
+    element.style.border = '';
+  });
 
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i].value === "") {
-            isAnyFieldEmpty = true;
-            // Add red border to the input field with an error
-            elements[i].style.border = '1px solid red';
-        }
+  let isAnyFieldEmpty = false; // Flag to check if any field is empty
+
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].value === "") {
+      isAnyFieldEmpty = true;
+      // Add red border to the input field with an error
+      elements[i].style.border = '1px solid red';
     }
+  }
 
-    if (isAnyFieldEmpty) {
-        errorMessages.push('Minden mezőt ki kell tölteni!');
-        hasErrors = true; // Set the flag to true if there is an error
-    }
-
+  if (isAnyFieldEmpty) {
+    errorMessages.push('Minden mezőt ki kell tölteni!');
+    hasErrors = true; // Set the flag to true if there is an error
+  }
+  
     if (!hasErrors) {
+      isFormSubmitted = true;
         const email = document.getElementById("email").value;
         const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
         if (!emailRegex.test(email)) {
@@ -137,14 +145,17 @@ function sendForm() {
             hasErrors = true; // Set the flag to true if there is an error
         }
 
-        const message = document.querySelector('.app-form-group.message input.app-form-control').value;
+        const message = document.querySelector('.app-form-group.message textarea.app-form-control').value;
         if (message.trim() === "") {
             errorMessages.push('Az üzenet mezőt ki kell tölteni!');
             // Add red border to the message input field
-            document.querySelector('.app-form-group.message input.app-form-control').style.border = '1px solid red';
+            document.querySelector('.app-form-group.message textarea.app-form-control').style.border = '1px solid red';
             hasErrors = true; // Set the flag to true if there is an error
         }
     }
+
+    if (!hasErrors) {
+    isFormSubmitted = true;
 
     if (hasErrors) {
         // Display all error messages
@@ -152,8 +163,13 @@ function sendForm() {
         return false;
     }
 
-    return true;
+    // Ha nincsenek hibák, akkor beküldjük az űrlapot
+    document.querySelector('form').submit();
+  } else {
+    // Display all error messages
+    alert(errorMessages.join('\n'));
+    return false;
+  }
 }
-
 
 </script>
