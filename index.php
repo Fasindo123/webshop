2e62560033eba2c6efb2036f255f84a4dd48335b
matethelol <mtszita2006@gmail.com>
2023-12-15 11:43:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -23,21 +23,21 @@ $conn->close();
       <?php require_once('components/header.php'); ?>
 
       <div class="container" id ="termekek">
-        <?php 
-          if ($items->num_rows > 0) {
+      <?php 
+        if ($items->num_rows > 0) {
             while ($item = $items->fetch_assoc()) {
-              echo '<div class="product-card col-6 col-sm-3" id="'.$item['id'].'">
+                echo '<a href="product.php?id='.$item['id'].'">
+                        <div class="product-card col-6 col-sm-3" id="'.$item['id'].'">
                         <img src="imgs/amd.jpg" alt="'.$item['name'].'" title="'.$item['name'].'">
                         <h2>'.$item['name'].'</h2>
                         <h4>Ár: '.$item['price'].' Ft</h4>
-            
                         <button><a href="#"><i class="fa-solid fa-heart-circle-plus"></i></a></button>
                         <button><a href="#"><i class="fa-solid fa-cart-plus"></i></a></button>
-                    </div>';
+                      </div></a>';
             }
-          } else {
+        } else {
             echo "Nincsenek termékek!";
-          }
+        }
         ?>
       </div>
       
