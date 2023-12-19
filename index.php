c49345035d8baf9d4bc50aa9a5d092821b9584a7
matethelol <mtszita2006@gmail.com>
2023-12-19 11:59:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $conn->close();
             while ($item = $items->fetch_assoc()) {
                 if (!$item['cover_img']) { $item['cover_img'] = "imgs/empty-photo.jpg"; } else { $item['cover_img'] = "dashboard/".$item['cover_img']; }
                 echo '<div class="product-card col-6 col-sm-6 col-md-6 col-lg-4" id="'.$item['id'].'">
-                      <a href="product.php?id='.$item['id'].'">
+                      <a href="product.php?id='.$item['id'].'" cat_id='.$item['category_id'].'>
                         <img src="'.$item['cover_img'].'" class="img-fluid" alt="' . $item['cover_img'] . '" title="' . $item['name'] . '">
                         <h2>' . $item['name'] . '</h2>
                       </a>
